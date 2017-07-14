4cc45932d48d10eac53aa9e2d76f1aac2dc99f4d
Einsten42 <milne.james@gmail.com>
2017-07-13 23:40:32 -0500

    Bumped to 1.0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='polyinterface',
-      version='1.0.5',
+      version='1.0.6',
       description='UDI Polyglot v2 Interface',
       url='https://github.com/Einstein42/udi-polyglot-interface',
       author='James Milne',
