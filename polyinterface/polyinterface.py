8f5e0bd2ed9f96072e9562de6070ac5d98b1e16e
Dmitri Kostukov <firstone@erissoft.com>
2019-01-16 17:53:04 -0500

    Removing features

--- polyinterface/polyinterface.py
+++ polyinterface/polyinterface.py
@@ -24,8 +24,6 @@ import select
 from threading import Thread
 import warnings
 
-from polyinterface import __features__
-
 
 def warning_on_one_line(message, category, filename, lineno, file=None, line=None):
     return '{}:{}: {}: {}'.format(filename, lineno, category.__name__, message)
@@ -220,8 +218,7 @@ class Interface(object):
             self._mqttc.publish(self.topicSelfConnection, json.dumps(
                 {
                     'connected': True,
-                    'node': self.profileNum,
-                    'features': __features__
+                    'node': self.profileNum
                 }), retain=True)
             LOGGER.info('Sent Connected message to Polyglot')
         else:
@@ -468,15 +465,7 @@ class Interface(object):
         self.inQueue.put(command)
 
     def supports_feature(self, feature):
-        if self.config is None:
-            return False
-
-        feature_support = self.config.get('features', {}).get(feature, 'off')
-        if feature_support == 'deprecated':
-            LOGGER.warning('Deprecated feature detected {}. Update interface and node server.'.format(feature))
-            return True
-
-        return feature_support == 'on'
+        return True
 
     def get_md_file_data(self, fileName):
         data = ''
@@ -486,9 +475,6 @@ class Interface(object):
         return data
 
     def send_custom_config_docs(self):
-        if not self.supports_feature('customParamsDoc'):
-            return
-
         data = ''
         if not self.custom_params_docs_file_sent:
             data = self.get_md_file_data(Interface.CUSTOM_CONFIG_DOCS_FILE_NAME)
@@ -661,7 +647,7 @@ class Controller(Node):
     """
     __exists = False
 
-    def __init__(self, poly):
+    def __init__(self, poly, name='Controller'):
         if self.__exists:
             warnings.warn('Only one Controller is allowed.')
             return
@@ -671,7 +657,7 @@ class Controller(Node):
             self.poly = poly
             self.poly.onConfig(self._gotConfig)
             self.poly.onStop(self.stop)
-            self.name = 'Controller'
+            self.name = name
             self.address = 'controller'
             self.primary = self.address
             self._drivers = deepcopy(self.drivers)
@@ -879,23 +865,10 @@ class Controller(Node):
         if not isinstance(data, basestring):
             LOGGER.error('addNotice: data isn\'t a string. Ignoring.')
         else:
-            if (self.poly.supports_feature('noticeByKey')):
-                self.poly.addNotice({ 'key': key, 'value': data})
-            else:
-                self.poly.addNotice(data)
+            self.poly.addNotice({ 'key': key, 'value': data})
 
     def removeNotice(self, key):
-        if (self.poly.supports_feature('noticeByKey')):
-            data = { 'key': str(key) }
-        else:
-            if not isinstance(key, int):
-                LOGGER.error('removeNotice: key isn\'t a int. Ignoring.')
-                return
-            try:
-                data = self.poly.config['notices'][key]
-            except (IndexError) as err:
-                LOGGER.error('Notices doesn\'t have an element at index {} ignoring. {}'.format(data, err), exc_info=True)
-                return
+        data = { 'key': str(key) }
         self.poly.removeNotice(data)
 
     def getNotices(self):
