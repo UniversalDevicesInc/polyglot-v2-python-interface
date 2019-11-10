a8d75108860275044b9b4da7fca6f131f32eb339
Einsten42 <milne.james@gmail.com>
2019-11-09 19:41:29 -0600

    2.0.32

--- polyinterface/__init__.py
+++ polyinterface/__init__.py
@@ -1,6 +1,6 @@
 from .polyinterface import Interface, Node, Controller, LOGGER, unload_interface
 
-__version__ = '2.0.31'
+__version__ = '2.0.32'
 __description__ = 'UDI Polyglot v2 Interface'
 __url__ = 'https://github.com/UniversalDevicesInc/polyglot-v2-python-interface'
 __author__ = 'James Milne'
