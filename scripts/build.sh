726d3b33acc8c6843773f7892c28a162b8397609
JimBo CA <jimboca3@gmail.com>
2020-04-08 18:49:52 -0700

    Move actions to makefile so they can be locally tested

--- scripts/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -x
-
-python3 setup.py sdist
-python3 setup.py bdist_wheel
--- scripts/publish.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -x
-#
-# For test.pypi.orig pass: -r https://test.pypi.org/pypi --skip-existing
-#
-
-twine upload $* dist/*
